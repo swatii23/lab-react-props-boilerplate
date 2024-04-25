3ca4ebcdcd5df2dc2f71dfd67eeabaed6e4f6272
Swati Mandal <swati.m_2000@rediffmail.com>
2024-04-25 15:05:50 +0530

    copy from jsx assignment

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import elephant from "./images/elephant.jpeg";
+
+const imageData = ()=>{
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+  return data;
+}
+
+function App() {
+  // code here
+  return (
+    <div>
+      <h2>Functional Component</h2>
+      <div className="image-div">
+        {imageData().map(image => (
+          <div>
+            <img key={image.id} src={image.img} alt={`image:${image.id}`} />          
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
